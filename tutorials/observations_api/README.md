b504b1e471ac7f772d489189adf93a3a1ed1bdce
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-04-11 14:19:29 +0200

    go

--- tutorials/observations_api/README.md
+++
@@ -1,5 +0,0 @@
-# Example: Observations API for data journalists
-
-http://cdb.io/XngAqa
-
-<script id='cartodb-1363704744429' src='http://ebtutorial.cartodb.com/tables/observations/embed_map.js?title=true&description=true&search=false&shareable=false&cartodb_logo=true&scrollwheel=true&sql=&sw_lat=55.637686135397544&sw_lon=12.43377685546875&ne_lat=55.72865675118282&ne_lon=12.76336669921875&height=400&id=cartodb-1363704744429'></script>
--- tutorials/observations_api/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-python pushobs.py --location 'Rådhuspladsen 37, 1785 København' -a f420d7a3f5a0e344f40ce20ecfc00edec675df42 -i journalist_42 'Der er her det sker!'
-#python pushobs.py --location 'Kongens nytorv, København' -a f420d7a3f5a0e344f40ce20ecfc00edec675df42 -i journalist_42 'Der er her det sker!'
-#python pushobs.py --location 'Mimersgade, København' -a f420d7a3f5a0e344f40ce20ecfc00edec675df42 -i journalist_42 'Der er her det sker!'
-#python pushobs.py --location 'Christiania, København' -a f420d7a3f5a0e344f40ce20ecfc00edec675df42 -i journalist_42 'Der er her det sker!'
-#python pushobs.py --location 'Valby langgade, København' -a f420d7a3f5a0e344f40ce20ecfc00edec675df42 -i journalist_42 'Der er her det sker!'
-#python pushobs.py --location 'Fuglebakkevej, Frederiksberg' -a f420d7a3f5a0e344f40ce20ecfc00edec675df42 -i journalist_43 'Der er her det sker!'
-
-# Se kort: http://cdb.io/XngAqa
